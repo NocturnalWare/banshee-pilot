e447e587dd51ff2b7d931bc10ce42b6d7c8c9872
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-17 20:37:12 -0500

    Updates Mobile after failed test.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,10 +37,10 @@
     <body>
         <div class="row">
             <div class="col-xs-12" style="height:100%;">
-                <iframe class="col-sm-12 hidden-sm hidden-md hidden-lg"width="560" height="315" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
-                <iframe class="col-sm-12 hidden-xs hidden-md hidden-lg"width="640" height="360" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
-                <iframe class="col-sm-12 hidden-xs hidden-sm hidden-lg"width="853" height="480" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
-                <iframe class="col-sm-12 hidden-xs hidden-sm hidden-md"width="1280" height="720" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-sm hidden-md hidden-lg" width="100%" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-xs hidden-md hidden-lg" width="640" height="360" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-xs hidden-sm hidden-lg" width="853" height="480" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-xs hidden-sm hidden-md" width="1280" height="720" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </body>
