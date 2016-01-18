83033714e768e1edcb87a7e076787ef8665327d8
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-17 20:24:08 -0500

    Banshee Pilot

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>The Banshee Pilot</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Cambay' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                background-color: #000;
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+        </style>
+    </head>
+    <body>
+        <div class="row">
+            <div class="col-xs-12" style="height:100%;">
+                <iframe class="col-sm-12 hidden-sm hidden-md hidden-lg"width="560" height="315" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-xs hidden-md hidden-lg"width="640" height="360" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-xs hidden-sm hidden-lg"width="853" height="480" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+                <iframe class="col-sm-12 hidden-xs hidden-sm hidden-md"width="1280" height="720" src="https://www.youtube.com/embed/9rDMz5vJfFk" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </body>
+</html>
